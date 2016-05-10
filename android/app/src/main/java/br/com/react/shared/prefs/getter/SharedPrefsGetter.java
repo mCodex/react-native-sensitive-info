778343ec8d430499a79813e702feb78cfcc87d41
Mateus Andrade <mat.andrade@live.com>
2016-05-10 14:19:40 -0300

    initial commit

---
+++ android/app/src/main/java/br/com/react/shared/prefs/getter/SharedPrefsGetter.java
@@ -0,0 +1,47 @@
+package br.com.react.shared.prefs.getter;
+
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.WritableNativeMap;
+import com.facebook.react.uimanager.IllegalViewOperationException;
+
+import java.util.Map;
+
+/**
+ * Created by classapp on 5/10/16.
+ */
+public class SharedPrefsGetter extends ReactContextBaseJavaModule {
+
+    public SharedPrefsGetter(ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
+
+    @Override
+    public String getName() {
+        return "SharedPrefsGetter";
+    }
+
+    @ReactMethod
+    public void getSharedPrefs(Callback cb) {
+        try {
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getReactApplicationContext());
+            Map<String, ?> allEntries = prefs.getAll();
+            WritableMap resultData = new WritableNativeMap();
+            SharedPreferences.Editor edit = prefs.edit();
+            edit.putString("luis", "coelho");
+            edit.commit();
+            for (Map.Entry<String, ?> entry : allEntries.entrySet()) {
+                resultData.putString(entry.getKey(), entry.getValue().toString());
+            }
+            cb.invoke(resultData);
+        } catch (IllegalViewOperationException e) {
+            cb.invoke(e.getMessage());
+        }
+    }
+}
