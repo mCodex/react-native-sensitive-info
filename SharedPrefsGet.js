a9dd4e60308b194074bbd40c21e8ed69a9a8dafc
Mateus Andrade <mat.andrade@live.com>
2016-05-12 17:12:18 -0300

    reverting

---
+++ SharedPrefsGet.js
@@ -0,0 +1,5 @@
+'use strict';
+
+import { NativeModules } from 'react-native';
+
+module.exports = NativeModules.SharedPrefsGetter;
--- index.android.js
+++ index.android.js
@@ -9,8 +9,54 @@
  });
  */
 
-'use strict';
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
 
-import { NativeModules } from 'react-native';
+import SharedPrefsGetter from './SharedPrefsGet';
 
-module.exports = NativeModules.SharedPrefsGetter;
+class SharedGetter extends Component {
+  render() {
+    SharedPrefsGetter.getSharedPrefs(function(data){
+      alert(data);
+    });
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.android.js
+        </Text>
+        <Text style={styles.instructions}>
+          Shake or press menu button for dev menu
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('SharedGetter', () => SharedGetter);
