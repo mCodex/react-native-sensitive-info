542205afff7a7d9a0b12a5648fa3787aa1f8ab77
Mateus Andrade <mat.andrade@live.com>
2016-05-18 16:06:52 -0300

    example refactor fixed

--- example/index.android.js
+++ example/index.android.js
@@ -11,22 +11,22 @@ import {
   View
 } from 'react-native';
 
-import SpfsManager from 'react-native-sensitive-info'; //<-- Add this line
+import RNSensitiveInfo from 'react-native-sensitive-info'; //<-- Add this line
 
 class example extends Component {
   render() {
 
-    SpfsManager.setPrefs('key1', 'value1');
-    SpfsManager.setPrefs('key2', 'value2');
-    SpfsManager.setPrefs('key3', 'value3');
-    SpfsManager.setPrefs('key4', 'value4');
-    SpfsManager.setPrefs('key5', 'value5');
+    RNSensitiveInfo.setPrefs('key1', 'value1');
+    RNSensitiveInfo.setPrefs('key2', 'value2');
+    RNSensitiveInfo.setPrefs('key3', 'value3');
+    RNSensitiveInfo.setPrefs('key4', 'value4');
+    RNSensitiveInfo.setPrefs('key5', 'value5');
 
-    SpfsManager.getKey('teste').then(function(data) {
+    RNSensitiveInfo.getPrefs('key1').then(function(data) {
         console.log(data);
     });
 
-    SpfsManager.getAllPrefs(function(result){
+    RNSensitiveInfo.getAllPrefs(function(result){
       console.log(result.key1);
     });
 
