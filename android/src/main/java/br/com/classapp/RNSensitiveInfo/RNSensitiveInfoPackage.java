3451848474a65df84b4447872659c3edc25f637d
Ulises Giacoman <ugiacoman@email.wm.edu>
2017-08-18 15:30:20 -0400

    Update deprecated createJSModules call
    Fix for react-native@0.47.1
    
    Relevant https://github.com/facebook/react-native/releases/tag/v0.47.0

--- android/src/main/java/br/com/classapp/RNSensitiveInfo/RNSensitiveInfoPackage.java
+++ android/src/main/java/br/com/classapp/RNSensitiveInfo/RNSensitiveInfoPackage.java
@@ -22,7 +22,6 @@ public class RNSensitiveInfoPackage implements ReactPackage {
         return modules;
     }
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
