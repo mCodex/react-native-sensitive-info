e0786e9877faa2ae663d7c26dcb82c398c9dc2ed
Victor Pedrazzi <vspedrazzi@gmail.com>
2018-10-23 20:56:45 -0300

    Update example project

--- example/android/app/src/main/java/com/example/MainApplication.java
+++ example/android/app/src/main/java/com/example/MainApplication.java
@@ -24,8 +24,13 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNSensitiveInfoPackage()
+          new RNSensitiveInfoPackage()
       );
+    }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
     }
   };
 
