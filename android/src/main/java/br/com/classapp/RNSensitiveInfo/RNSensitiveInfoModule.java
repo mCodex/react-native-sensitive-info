431803db3a08dc8a482a2e752da79c25b15bcec9
Andrej Česen <andrej.cesen@me.com>
2019-02-05 10:12:00 +0100

    Fixed crashing on SDK < Marshamllow

--- android/src/main/java/br/com/classapp/RNSensitiveInfo/RNSensitiveInfoModule.java
+++ android/src/main/java/br/com/classapp/RNSensitiveInfo/RNSensitiveInfoModule.java
@@ -226,30 +226,32 @@ public class RNSensitiveInfoModule extends ReactContextBaseJavaModule {
         }
     }
 
-    private void showDialog(final HashMap strings, FingerprintManager.CryptoObject cryptoObject, FingerprintUiHelper.Callback callback) {
-        // DialogFragment.show() will take care of adding the fragment
-        // in a transaction.  We also want to remove any currently showing
-        // dialog, so make our own transaction and take care of that here.
+    private void showDialog(final HashMap strings, Object cryptoObject, FingerprintUiHelper.Callback callback) {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            // DialogFragment.show() will take care of adding the fragment
+            // in a transaction.  We also want to remove any currently showing
+            // dialog, so make our own transaction and take care of that here.
 
-        Activity activity = getCurrentActivity();
-        if (activity == null) {
-            callback.onError(AppConstants.E_INIT_FAILURE,
-                    strings.containsKey("cancelled") ? strings.get("cancelled").toString() : "Authentication was cancelled");
-            return;
+            Activity activity = getCurrentActivity();
+            if (activity == null) {
+                callback.onError(AppConstants.E_INIT_FAILURE,
+                        strings.containsKey("cancelled") ? strings.get("cancelled").toString() : "Authentication was cancelled");
+                return;
+            }
+
+            FragmentTransaction ft = activity.getFragmentManager().beginTransaction();
+            Fragment prev = getCurrentActivity().getFragmentManager().findFragmentByTag(AppConstants.DIALOG_FRAGMENT_TAG);
+            if (prev != null) {
+                ft.remove(prev);
+            }
+            ft.addToBackStack(null);
+
+            // Create and show the dialog.
+            FingerprintAuthenticationDialogFragment newFragment = FingerprintAuthenticationDialogFragment.newInstance(strings);
+            newFragment.setCryptoObject((FingerprintManager.CryptoObject) cryptoObject);
+            newFragment.setCallback(callback);
+            newFragment.show(ft, AppConstants.DIALOG_FRAGMENT_TAG);
         }
-
-        FragmentTransaction ft = activity.getFragmentManager().beginTransaction();
-        Fragment prev = getCurrentActivity().getFragmentManager().findFragmentByTag(AppConstants.DIALOG_FRAGMENT_TAG);
-        if (prev != null) {
-            ft.remove(prev);
-        }
-        ft.addToBackStack(null);
-
-        // Create and show the dialog.
-        FingerprintAuthenticationDialogFragment newFragment = FingerprintAuthenticationDialogFragment.newInstance(strings);
-        newFragment.setCryptoObject(cryptoObject);
-        newFragment.setCallback(callback);
-        newFragment.show(ft, AppConstants.DIALOG_FRAGMENT_TAG);
     }
 
     /**
