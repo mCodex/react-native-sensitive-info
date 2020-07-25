2a869af91c4702635cbaafe16f76acfd03275d8b
Mateus Andrade <mat.andrade@live.com>
2020-07-24 21:18:53 -0300

    working on example home page

--- example/index.js
+++ example/index.js
@@ -1,9 +1,9 @@
-/**
- * @format
- */
+import 'react-native-gesture-handler';
 
-import {AppRegistry} from 'react-native';
-import App from './src/pages/Homes/Home';
-import {name as appName} from './app.json';
+import { AppRegistry } from 'react-native';
+
+import App from './src/routes';
+
+import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
--- example/src/pages/Home/index.tsx
+++ example/src/pages/Home/index.tsx
@@ -1,19 +1,61 @@
-import React, { useCallback } from 'react'
-import {View, Button} from 'react-native'
-import SInfo from 'react-native-sensitive-info'
+import React, { useCallback } from 'react';
+import { View, Button, Alert } from 'react-native';
+import SInfo from 'react-native-sensitive-info';
 
 const Home: React.FC = () => {
-    const handleAddItemOnPress = useCallback(() => {
-        SInfo.setItem('key1', 'value1', {
-            sharedPreferencesName: 'exampleApp',
-            keychainService: 'exampleApp'
-        })
-    },[])
+  const handleAddUsingSetItemOnPress = useCallback(() => {
+    SInfo.setItem('key1', 'value1', {
+      sharedPreferencesName: 'exampleApp',
+      keychainService: 'exampleApp',
+    });
+  }, []);
 
-    return (
-      <View>
-        <Button title="Add Item" onPress={handleAddItemOnPress} />
-      </View>
-    );
-}
+  const handleSetItemUsingTouchIDOnPress = useCallback(async () => {
+    try {
+      const deviceHasSensor = await SInfo.isSensorAvailable();
+
+      if (!deviceHasSensor) {
+        return Alert.alert('No sensor found');
+      }
+
+      SInfo.setItem('touchIdItem', 'dataStoredUsingTouchId', {
+        // touchID: true,
+        showModal: true
+      });
+    } catch (ex) {
+      console.log(ex.message);
+    }
+  }, []);
+
+  /**
+   * @BUG when triggering this function iOS emulator crashes. Need to investigate
+   */
+  const getTouchIDItem = useCallback(async () => {
+    const deviceHasSensor = await SInfo.isSensorAvailable();
+
+    if (!deviceHasSensor) {
+      return Alert.alert('No sensor found');
+    }
+
+    SInfo.getItem('touchIdItem', {
+      touchID: true,
+      showModal: true
+    });
+  }, []);
+
+  return (
+    <View>
+      <Button
+        title="Add item using setItem"
+        onPress={handleAddUsingSetItemOnPress}
+      />
+      <Button
+        title="Add item using TouchID"
+        onPress={handleSetItemUsingTouchIDOnPress}
+      />
+
+      <Button title="Get TouchID Data" onPress={getTouchIDItem} />
+    </View>
+  );
+};
 export default Home;
