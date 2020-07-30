6b78dc2e19e45a4aa2191968717f3f1301ec923b
Mateus Andrade <mat.andrade@live.com>
2020-07-30 14:06:13 -0300

    Modifying docusaurus editing url

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -1,47 +1,47 @@
 module.exports = {
-  title: 'React-Native-Sensitive-Info',
-  tagline: 'A secure storage for React-Native apps',
-  url: 'https://mcodex.github.io', // Your website URL
-  baseUrl: '/react-native-sensitive-info/', // Base URL for your project */
-  favicon: 'img/favicon.ico',
-  organizationName: 'mcodex', // Usually your GitHub org/user name.
-  projectName: 'react-native-sensitive-info', // Usually your repo name.
+  title: "React-Native-Sensitive-Info",
+  tagline: "A secure storage for React-Native apps",
+  url: "https://mcodex.github.io", // Your website URL
+  baseUrl: "/react-native-sensitive-info/", // Base URL for your project */
+  favicon: "img/favicon.ico",
+  organizationName: "mcodex", // Usually your GitHub org/user name.
+  projectName: "react-native-sensitive-info", // Usually your repo name.
   themeConfig: {
     googleAnalytics: {
-      trackingID: 'UA-79205996-6',
+      trackingID: "UA-79205996-6",
     },
     algolia: {
-      apiKey: 'db98b41e2171bc1f197a03a254c46b7b',
-      indexName: 'react-native-sensitive-info',
+      apiKey: "db98b41e2171bc1f197a03a254c46b7b",
+      indexName: "react-native-sensitive-info",
     },
     navbar: {
-      title: 'RNSInfo',
+      title: "RNSInfo",
       logo: {
-        alt: 'react-native-sensitive-info',
-        src: 'img/logo.png',
+        alt: "react-native-sensitive-info",
+        src: "img/logo.png",
       },
       items: [
         {
-          to: 'docs/',
-          activeBasePath: 'docs',
-          label: 'Docs',
-          position: 'left',
+          to: "docs/",
+          activeBasePath: "docs",
+          label: "Docs",
+          position: "left",
         },
         {
-          to: 'docs/5.x',
-          activeBasePath: 'docs/5.x',
-          label: 'v5.x',
-          position: 'left',
+          to: "docs/5.x",
+          activeBasePath: "docs/5.x",
+          label: "v5.x",
+          position: "left",
         },
         {
-          href: 'https://github.com/mcodex/react-native-sensitive-info',
-          label: 'GitHub',
-          position: 'right',
+          href: "https://github.com/mcodex/react-native-sensitive-info",
+          label: "GitHub",
+          position: "right",
         },
       ],
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       // links: [
       // {
       //   title: 'Docs',
@@ -92,18 +92,18 @@ module.exports = {
   },
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
         docs: {
-          homePageId: 'overview',
-          sidebarPath: require.resolve('./sidebars.js'),
+          homePageId: "overview",
+          sidebarPath: require.resolve("./sidebars.js"),
           editUrl:
-            'https://github.com/mCodex/react-native-sensitive-info/tree/master/website',
+            "https://github.com/mCodex/react-native-sensitive-info/tree/website/website",
           showLastUpdateAuthor: true,
           showLastUpdateTime: true,
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       },
     ],
