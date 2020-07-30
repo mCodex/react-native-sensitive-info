bb9ef047d2bb16bdfe784d56e7f205d2059b15f7
Alejandro <maggialejs@gmail.com>
2020-07-30 11:35:03 -0300

    fix(android): key user not authenticated (#224)
    Co-authored-by: Alejandro <alejandro.maggi@recargapay.com>

--- android/src/main/java/br/com/classapp/RNSensitiveInfo/utils/AppConstants.java
+++ android/src/main/java/br/com/classapp/RNSensitiveInfo/utils/AppConstants.java
@@ -5,6 +5,7 @@ public interface AppConstants {
     String DIALOG_FRAGMENT_TAG = "authFragment";
 
     // error codes
+    String KM_ERROR_KEY_USER_NOT_AUTHENTICATED = "KM_ERROR_KEY_USER_NOT_AUTHENTICATED";
     String E_AUTHENTICATION_CANCELLED = "E_AUTHENTICATION_CANCELLED";
     String E_INIT_FAILURE = "E_INIT_FAILURE";
 }
