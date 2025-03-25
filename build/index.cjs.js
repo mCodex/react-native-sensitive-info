cbe57833de66e1981dea5099eb6db5bda7e66d8f
davortrifunov <davortrifunov96@hotmail.com>
2025-03-25 10:53:49 +0100

    rimraf, yarn

---
+++ build/index.cjs.js
@@ -0,0 +1,22 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var reactNative = require('react-native');
+
+var RNSensitiveInfo = reactNative.NativeModules.RNSensitiveInfo;
+RNSensitiveInfo.setInvalidatedByBiometricEnrollment = function (invalidatedByBiometricEnrollment) {
+    if (RNSensitiveInfo.setInvalidatedByBiometricEnrollment == null) {
+        return undefined;
+    }
+    return RNSensitiveInfo.setInvalidatedByBiometricEnrollment(invalidatedByBiometricEnrollment);
+};
+RNSensitiveInfo.cancelFingerprintAuth = function () {
+    if (RNSensitiveInfo.cancelFingerprintAuth == null) {
+        return undefined;
+    }
+    return RNSensitiveInfo.cancelFingerprintAuth();
+};
+
+exports["default"] = RNSensitiveInfo;
+//# sourceMappingURL=index.cjs.js.map
---
+++ build/index.es.js
@@ -0,0 +1,18 @@
+import { NativeModules } from 'react-native';
+
+var RNSensitiveInfo = NativeModules.RNSensitiveInfo;
+RNSensitiveInfo.setInvalidatedByBiometricEnrollment = function (invalidatedByBiometricEnrollment) {
+    if (RNSensitiveInfo.setInvalidatedByBiometricEnrollment == null) {
+        return undefined;
+    }
+    return RNSensitiveInfo.setInvalidatedByBiometricEnrollment(invalidatedByBiometricEnrollment);
+};
+RNSensitiveInfo.cancelFingerprintAuth = function () {
+    if (RNSensitiveInfo.cancelFingerprintAuth == null) {
+        return undefined;
+    }
+    return RNSensitiveInfo.cancelFingerprintAuth();
+};
+
+export { RNSensitiveInfo as default };
+//# sourceMappingURL=index.es.js.map
