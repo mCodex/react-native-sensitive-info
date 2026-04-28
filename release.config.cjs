09d80350f39b37f630c4a35954825b32e3af56c1
Mateus Andrade <mat.andrade@live.com>
2026-04-28 12:25:29 -0300

    Fix branch name in release configuration from 'main' to 'master'

--- release.config.cjs
+++ release.config.cjs
@@ -15,7 +15,7 @@ const sortMap = Object.fromEntries(
  * @type {import('semantic-release').GlobalConfig}
  */
 module.exports = {
-	branches: ['main', { name: 'next', prerelease: 'next' }],
+	branches: ['master', { name: 'next', prerelease: 'next' }],
 	plugins: [
 		[
 			'@semantic-release/commit-analyzer',
